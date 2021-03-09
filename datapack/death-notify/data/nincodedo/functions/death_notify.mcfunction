fa68d00cc30012dd9f4193d9b150e7f311de044c
Nincodedo <nincodedo@gmail.com>
2021-03-08 21:37:20 -0600

    Add lightning death

--- datapack/death-notify/data/nincodedo/functions/death_notify.mcfunction
+++ datapack/death-notify/data/nincodedo/functions/death_notify.mcfunction
@@ -1,1 +1,8 @@
-say I did this
+title @p[advancements={nincodedo:death_by_entity=true}] title {"text":"you did this", "color": "red"}
+title @p[advancements={nincodedo:death_by_entity=false}] title {"selector": "@p", "color": "red", "extra": [" did this"]}
+execute as @p run summon minecraft:lightning_bolt ~ ~ ~
+execute as @p run summon minecraft:lightning_bolt ~10 ~ ~
+execute as @p run summon minecraft:lightning_bolt ~ ~10 ~
+execute as @p run summon minecraft:lightning_bolt ~ ~ ~10
+execute as @p run summon minecraft:lightning_bolt ~5 ~ ~
+weather thunder
--- scripts/resetWorld.sh
+++ scripts/resetWorld.sh
@@ -3,6 +3,12 @@ rcon_command(){
   docker exec mc rcon-cli $1
 }
 
+scoreboard_setup(){
+  rcon_command "scoreboard objectives add health health"
+  rcon_command "scoreboard objectives setdisplay list health"  
+  rcon_command "scoreboard objectives modify health rendertype hearts"
+}
+
 cd /mnt/e/Docker/minecraft
 echo "Starting script in: `pwd`"
 server_log=logs/latest.log
@@ -17,20 +23,23 @@ do
     sleep 0.1
     docker ps -f name=mc | grep healthy > /dev/null
   done
-  echo "Found, tailing log"
+  scoreboard_setup
   seed=`rcon_command seed`
-  echo "`date +\"%d-%m-%Y %l:%M %p\"` $seed" >> seed.log
+  current_datetime=`date +"%d-%m-%Y %H:%m %p"`
+  echo "$current_datetime $seed" >> seed.log
+  echo "Found, tailing log"
   ( tail -f -n0 "$server_log" & ) | grep -q "$grep_phrase"
   dead_player=`tail -n 10 "$server_log" | grep "$grep_phrase" | awk '{print $4}'`
+  current_datetime=`date +"%d-%m-%Y %H:%m %p"`
   echo "$dead_player died, restarting server"
   rcon_command "say $dead_player has died, the server is restarting in $death_reset_delay_seconds seconds"
-  echo "`date +\"%d-%m-%Y %H:%m %p\"` $dead_player died" >> death.log
-  echo "MOTD=OCW Minecraft\nLast incident `date +\"%d-%m-%Y %H:%m %p\"`" > /app/ocw-minecraft/docker.env
+  echo "$current_datetime $dead_player died" >> death.log
+  echo "MOTD=OCW Minecraft\nLast incident $current_datetime" > /app/ocw-minecraft/docker.env
   sleep $death_reset_delay_seconds
   docker stop mc
   docker rm mc
   rm -rf world
   mkdir world world/datapacks
   cp -r /app/death-notify/ world/datapacks/
-  docker-compose -f /app/ocw-minecraft/docker-compose.yaml up -d mc-java
+  docker-compose -f /app/ocw-minecraft/docker-compose.yaml up -d mc
 done  
