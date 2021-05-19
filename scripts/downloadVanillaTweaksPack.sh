52fdffbcb75d6c6a6277691c472d639dab8fecbf
Nincodedo <Nincodedo@users.noreply.github.com>
2021-05-19 14:50:15 -0500

    Add VanillaTweaks datapack (#14)

---
+++ scripts/downloadVanillaTweaksPack.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+packResponse=`curl -k --request POST \
+  --url https://vanillatweaks.net/assets/server/zipcraftingtweaks.php \
+  --header 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' \
+  --data 'packs={"quality of life":["sandstone dyeing","universal dyeing"],"unpackables":["unpackable ice","unpackable nether wart","unpackable wool"]}' \
+  --data version=1.16`
+
+packDownloadUrl=https://vanillatweaks.net`echo $packResponse | jq '.["link"]' | sed -e 's/^"//' -e 's/"$//'`
+
+wget -O $1/VanillaTweaks.zip $packDownloadUrl
