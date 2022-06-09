4be3b98230d54f7781a920e97a299ccd7db4ede2
Nincodedo <Nincodedo@users.noreply.github.com>
2022-06-09 08:21:38 -1000

    Release 5.0: Minecraft 1.19 (#34)

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # OCW Minecraft Servers [![Main Build](https://github.com/Nincodedo/rhc-ocw/actions/workflows/build.yml/badge.svg)](https://github.com/Nincodedo/rhc-ocw/actions/workflows/build.yml)
 
-This is the current server orchestration for OCW's Minecraft servers. It uses [itzg's Minecraft Docker image](https://github.com/itzg/docker-minecraft-server).
+This is the current server orchestration for OCW's Minecraft servers. It
+uses [itzg's Minecraft Docker image](https://github.com/itzg/docker-minecraft-server).
 
 ## Minecraft Radical Hardcore
-Radical Hardcore (RHC) starts up a Minecraft server in Hardcore mode with one additional rule. If anyone dies, the entire server is wiped and started anew.
+
+Radical Hardcore (RHC) starts up a Minecraft server in Hardcore mode with one additional rule. If anyone dies, the
+entire server is wiped and started anew.
 
 ## Minecraft Manhunt
-Minecraft Manhunt is based off Dream's Minecraft Manhunt Youtube series. Currently uses [this Spigot plugin](https://www.spigotmc.org/resources/manhunt-1-16x-1-18x.86708/) for game management and [a log watcher](Dockerfile-log-watcher-manhunt) for restarting the server after games are completed.
+
+Minecraft Manhunt is based off Dream's Minecraft Manhunt Youtube series. Currently
+uses [this Spigot plugin](https://www.spigotmc.org/resources/manhunt-1-16x-1-18x.86708/) for game management
+and [a log watcher](Dockerfile-log-watcher-manhunt) for restarting the server after games are completed.
--- scripts/resetWorld-manhunt.sh
+++ scripts/resetWorld-manhunt.sh
@@ -1,43 +1,41 @@
 #!/bin/sh
 
 rcon_command() {
-  docker exec $minecraft_docker_container_name rcon-cli $1
+  docker exec "$minecraft_docker_container_name" rcon-cli --password minecraft --port 25575 "$1"
 }
 
 log() {
-  log_time=`date +"%d-%m-%Y %I:%M:%S %p"`
-  echo "$log_time [LogWatcher]: $@"
+  log_time=$(date +"%d-%m-%Y %I:%M:%S %p")
+  echo "$log_time [LogWatcher]: $*"
 }
 
-world_ending_announcements() {
-    sleep $death_reset_delay_seconds
-    docker stop $minecraft_docker_container_name
-    docker rm $minecraft_docker_container_name
-    docker-compose -f $minecraft_compose_dir/docker-compose.yaml up -d $minecraft_docker_container_name
+restart_world() {
+  docker stop "$minecraft_docker_container_name"
+  docker rm "$minecraft_docker_container_name"
+  docker-compose -f "$minecraft_compose_dir"/docker-compose.yaml up -d "$minecraft_docker_container_name"
 }
 
-minecraft_server_dir=/data
 app_dir=/app
 minecraft_compose_dir=$app_dir/ocw-minecraft
-minecraft_server_log=$minecraft_server_dir/logs/latest.log
 minecraft_docker_container_name=mc-manhunt
 death_reset_delay_seconds=20
 grep_phrase="\[.* INFO\]: Hunters win! Do \/huntplus start to play again!"
 
-log "Starting LogWatcher in: `pwd`"
-while : ;
-do
+log "Starting LogWatcher in: $(pwd)"
+while :; do
   log "Checking for healthy container status"
-  docker ps -f name=$minecraft_docker_container_name | grep healthy > /dev/null
+  docker ps -f name=$minecraft_docker_container_name | grep healthy >/dev/null
   checkHealth=$?
-  until [ $checkHealth -eq 0 ];
-  do
+  until [ $checkHealth -eq 0 ]; do
     sleep 0.1
-    docker ps -f name=$minecraft_docker_container_name | grep healthy > /dev/null
+    docker ps -f name=$minecraft_docker_container_name | grep healthy >/dev/null
     checkHealth=$?
   done
   log "Found healthy container, tailing docker log"
-  ( docker logs $minecraft_docker_container_name --tail 0 -f & ) | grep -q "$grep_phrase"
-  log "Game has ended. Restarting in $death_reset_delay_seconds seconds"
-  world_ending_announcements
+  (docker logs $minecraft_docker_container_name --tail 0 -f &) | grep -q "$grep_phrase"
+  log "Game has ended. Waiting for all players to disconnect."
+  rcon_command "tellraw @a {\"text\":\"The server will be restarted after everyone disconnects\"}"
+  rcon_command "tellraw @a [{\"text\":\"Switch to spectator mode? \"},{\"text\":\"Click here\",\"underlined\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gamemode spectator\"}}]"
+  (docker logs $minecraft_docker_container_name --tail 0 -f &) | grep -q "\[Autopause loop\] All clients disconnected"
+  restart_world
 done
