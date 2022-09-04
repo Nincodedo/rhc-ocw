bc1f855dd12d6f727d41fb39ec42ea4a36385358
Nincodedo <nincodedo@gmail.com>
2022-09-04 11:46:38 -0500

    Update Tekkit 2 to v1.0.1

--- scripts/downloadTekkit2.sh
+++ scripts/downloadTekkit2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir /server
 cd /server
-wget -O tekkit.zip https://servers.technicpack.net/Technic/servers/tekkit-2/Tekkit-2_Server_v1.0.0.zip
+wget -O tekkit.zip https://servers.technicpack.net/Technic/servers/tekkit-2/Tekkit-2_Server_v1.0.1.zip
 
 unzip tekkit.zip mods/* config/* scripts/*
