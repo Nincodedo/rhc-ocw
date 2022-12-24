5b9feedcd06e54941f797d3925a25cf7245cbd69
Nincodedo <nincodedo@gmail.com>
2022-12-24 00:46:08 -0600

    Remove left over Tekkit 2 files

--- scripts/downloadTekkit2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mkdir /server
-cd /server
-wget -O tekkit.zip https://servers.technicpack.net/Technic/servers/tekkit-2/Tekkit-2_Server_v1.0.5.zip
-
-unzip tekkit.zip mods/* config/* scripts/*
